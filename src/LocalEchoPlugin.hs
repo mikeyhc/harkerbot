f17556826019554fa4ee6b760773c6293b427d2b
Michael Blockley <michael@sss.co.nz>
2014-08-11 17:25:55 +1200

    wip/refactor to harkerirc

--- src/LocalEchoPlugin.hs
+++ src/LocalEchoPlugin.hs
@@ -11,55 +11,10 @@ import System.Directory
 import System.Exit
 import System.IO
 
-data QuitException = QuitException String
-    deriving (Typeable, Show)
-instance Exception QuitException
-
 sockaddr = "/tmp/.local-echo.sock"
 
-main = do
-    s <- listenOn (UnixSocket sockaddr)
-    h <- connectTo "localhost" (UnixSocket "/tmp/.harker-server.sock")
-    hPutStrLn h "name: local-echo"
-    hPutStrLn h "version: 0.1.0.0"
-    hPutStrLn h $ "server: " ++ sockaddr
-    r <- hGetLine h
-    if r == "registered" 
-        then do
-            tid <- myThreadId
-            loopfunc (acceptfunc s tid) `catch` quitcatch
-        else hPutStrLn stderr $ "error: " ++ r
-    b <- doesFileExist sockaddr 
-    when b $ removeFile sockaddr
+main = runPlugin "local-echo" "0.1.0.0" acceptFunc
 
-
-quitcatch :: QuitException -> IO ()
-quitcatch _ = do 
-    putStrLn "quiting"
-    return ()
-
-trim [x] 
-    | x == '\n' = []
-    | otherwise = [x]
-trim (x:xs) = x:trim xs
-
-loopfunc a = a >> loopfunc a
-
-acceptfunc :: Socket -> ThreadId -> IO ()
-acceptfunc s tid = accept s >>= \(h, _, _) ->
-    void $ forkFinally (forkfunc h tid) (exitfunc h)
-
-exitfunc h _ = hClose h
-
-forkfunc :: Handle -> ThreadId -> IO ()
-forkfunc h tid = do
-    l <- trim <$> hGetLine h
-    if l == "action: quit" 
-        then exitfunc h (Right ()) >> throwTo tid (QuitException "done")
-        else do
-            putStrLn l
-            echo h l
-            forkfunc h tid
 
 echo :: Handle -> String -> IO ()
 echo h m 
